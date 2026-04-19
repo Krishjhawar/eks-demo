0037ab45e76deaa783ec11a77007059bcd26de94
KrishJhawar <krish.jhawar2020@gmail.com>
2026-04-19 20:32:47 +0530

    Change node instance to t3.small

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,7 +37,7 @@ variable "private_subnets" {
 variable "node_instance_type" {
   description = "EC2 instance type for EKS worker nodes"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.small"
 }
 
 variable "node_min_size" {
